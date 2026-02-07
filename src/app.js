6dc0fbfb57538fad50f8b8e23619f78f92df6c57
Angelica Aimee <anherikaplay@gmail.com>
2026-02-07 10:33:07 -0600

    Ajustando cositas

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
-import express from "express";
-import productsRouter from "./routes/products.router.js";
-import cartsRouter from "./routes/carts.router.js";
+import express from 'express';
+import productsRouter from './routes/products.router.js';
+import cartsRouter from './routes/carts.router.js';
 
 const app = express();
+const PORT = 8080;
 
+// Middlewares:
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/products", productsRouter);
-app.use("/api/carts", cartsRouter); }
+// Rutas base
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
 
-const PORT = 8080;
-app.listen(PORT, () =>
-	console.log(`Servidor escuchando en http://localhost:${PORT}`),
-);
+// El servidor escucha
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
